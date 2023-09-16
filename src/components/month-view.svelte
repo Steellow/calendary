7926e6e31e5965669566d82e1f8523aa6eff7914
Hannes Kinnunen <hankilapio@gmail.com>
2023-09-16 11:41:29 +0300

    Change calendar view from table to grid

--- src/components/month-view.svelte
+++ src/components/month-view.svelte
@@ -7,28 +7,22 @@
 </script>
 
 <div class="canvas p-10">
-	<table class="h-full w-full table-fixed">
-		<thead>
-			<tr>
-				{#each ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'] as weekday}
-					<th class="border border-black">{weekday}</th>
-				{/each}
-			</tr>
-		</thead>
-		<tbody>
-			{#each allWeekRows as row, i ('row' + i)}
-				<tr>
-					{#each row as cell, idx ('cell' + idx)}
-						<td
-							class="mx-auto border border-black text-center text-xl font-bold {isWeekend(idx)
-								? 'text-red-600'
-								: ''}">{cell}</td
-						>
-					{/each}
-				</tr>
+	<div class="grid grid-cols-7">
+		{#each ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] as weekday}
+			<div class="h-min font-bold">{weekday}</div>
+		{/each}
+	</div>
+
+	<!-- Border also here so outlines aren't thinner than grid lines -->
+	<div class="grid h-full grid-cols-7 border">
+		{#each allWeekRows as row, i ('row' + i)}
+			{#each row as cell, idx ('cell' + idx)}
+				<div class="border p-1 text-lg {isWeekend(idx) ? 'text-red-600' : ''}">
+					{cell === '_' ? '' : cell}
+				</div>
 			{/each}
-		</tbody>
-	</table>
+		{/each}
+	</div>
 </div>
 
 <style>
