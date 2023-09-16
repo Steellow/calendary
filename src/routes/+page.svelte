ff57ac07f79456be2f0e9ad94c3e1a32bda056cc
Hannes Kinnunen <hankilapio@gmail.com>
2023-09-16 12:11:40 +0300

    Rename components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 
-	import MonthView from '../components/month-view.svelte';
-	import PageSelector from '../components/page-selector.svelte';
+	import Canvas from '../components/canvas.svelte';
+	import Options from '../components/options.svelte';
 
 	let year: number = 2023;
 	let month: number = 9;
 </script>
 
 <div class="mx-auto my-6 flex max-w-screen-xl">
-	<PageSelector bind:year bind:month />
-	<MonthView {year} {month} />
+	<Options bind:year bind:month />
+	<Canvas {year} {month} />
 </div>
