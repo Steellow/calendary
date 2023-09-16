04c7eab1c7179713fbfd24a26f06188610e440dd
Hannes Kinnunen <hankilapio@gmail.com>
2023-09-16 10:39:25 +0300

    Fix bind

--- src/components/page-selector.svelte
+++ src/components/page-selector.svelte
@@ -5,9 +5,9 @@
 	export let month: number;
 </script>
 
-<div class="w-1/3">
-	<form class=" flex space-x-4">
-		<Input label="Year" value={year} />
-		<Input label="Month" value={month} />
+<div class="mt-10 w-1/3">
+	<form class="flex space-x-4">
+		<Input label="Year" bind:value={year} />
+		<Input label="Month" bind:value={month} />
 	</form>
 </div>
