5083106f52183803c50484c18423fe65440a8086
Hannes Kinnunen <hankilapio@gmail.com>
2023-09-27 15:47:03 +0300

    Save img instead of opening print dialog
    Still super WIP, but couldn't get native print dialog to scale stuff well
    
    I'll try to save calendar as img → convert img to pdf → download pdf

--- src/components/print-button.svelte
+++ src/components/print-button.svelte
@@ -1,5 +1,26 @@
+<script lang="ts">
+	import html2canvas from 'html2canvas';
+
+	const print = () => {
+		const printable = document.querySelector('#printable') as HTMLElement;
+
+		const copy = printable?.cloneNode(true) as HTMLElement;
+		printable.style.transform = 'none';
+
+		html2canvas(printable).then((canvas) => {
+			canvas.toBlob((blob) => {
+				const link = document.createElement('a');
+				link.download = 'test.png';
+				link.href = URL.createObjectURL(blob as Blob);
+				link.click();
+				URL.revokeObjectURL(link.href);
+			});
+		});
+	};
+</script>
+
 <button
-	on:click={() => window.print()}
+	on:click|preventDefault={print}
 	class="w-full rounded border border-gray-400 px-4 py-2 font-semibold text-gray-800 shadow hover:bg-red-100"
 >
 	Print 🖨️
