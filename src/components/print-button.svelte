89c4b0ed08a3d7e0caa60bc0798c1469197c48d5
Hannes Kinnunen <hankilapio@gmail.com>
2023-10-18 08:30:25 +0300

    Don't change preview size when printing (visual bug)

--- src/components/print-button.svelte
+++ src/components/print-button.svelte
@@ -4,18 +4,26 @@
 	const print = () => {
 		const printable = document.querySelector('#printable') as HTMLElement;
 
+		// Create copy so the actual preview doesn't change size
 		const copy = printable?.cloneNode(true) as HTMLElement;
-		printable.style.transform = 'none';
+		copy.style.transform = 'none';
 
-		html2canvas(printable).then((canvas) => {
-			canvas.toBlob((blob) => {
-				const link = document.createElement('a');
-				link.download = 'test.png';
-				link.href = URL.createObjectURL(blob as Blob);
-				link.click();
-				URL.revokeObjectURL(link.href);
+		// Element must be in DOM for html2canvas to work
+		document.body.appendChild(copy);
+
+		html2canvas(printable)
+			.then((canvas) => {
+				canvas.toBlob((blob) => {
+					const link = document.createElement('a');
+					link.download = 'test.png';
+					link.href = URL.createObjectURL(blob as Blob);
+					link.click();
+					URL.revokeObjectURL(link.href);
+				});
+			})
+			.then(() => {
+				copy.remove();
 			});
-		});
 	};
 </script>
 
